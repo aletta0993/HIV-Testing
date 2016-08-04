8b592fd89b52ef2785926b0377e46fec97c071f6
katielong <yl3239@columbia.edu>
2016-08-04 16:35:50 -0400

    change format

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-var chart = d3.parsets().dimensions(["HIV Test Today","Religious Level","HIV Tested Last Year?","Ever HIV Tested?",
-	"HIV Info on Billboards?","Time in the US","Unprotected Sex","Education", "Live with Partner"]);
+var chart = d3.parsets()
+				.dimensions(["HIV Test Today","Religious Level","HIV Tested Last Year?","Ever HIV Tested?",
+							"HIV Info on Billboards?","Time in the US","Unprotected Sex","Education", "Live with Partner"])
+				.width(650);
 
-var vis = d3.select("#vis").append("svg")
+var vis = d3.select("#content").append("svg")
     .attr("width", chart.width())
     .attr("height", chart.height());
 
-// var vis_relig = d3.select(".sub").append("svg")
-//    .attr("width", chart.width())
-//    .attr("height", chart.height());
-
 d3.csv("viz.csv", function(error, csv) {
 	vis.datum(csv).call(chart);
-//	vis_relig.datum(csv).call(d3.parsets().dimensions(["HIV Test Today", "Religious Level"]));
 });
 
 

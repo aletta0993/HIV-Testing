c848765b9113fb1dd3701233baec9ca5fc50e7bc
katielong <yl3239@columbia.edu>
2016-09-05 16:58:40 -0400

    plugin to beautify

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 var chart = d3.parsets()
-				.dimensions(["HIV Test Today","Religious Level","HIV Tested Last Year?","Ever HIV Tested?",
-							"HIV Info on Billboards?","Time in the US","Unprotected Sex","Education", "Live with Partner"])
-				.width(650);
+    .dimensions(["HIV Test Today", "Religious Level", "HIV Tested Last Year?", "Ever HIV Tested?",
+        "HIV Info on Billboards?", "Time in the US", "Unprotected Sex", "Education", "Live with Partner"
+    ])
+    .width(650);
 
 var vis = d3.select("#content").append("svg")
     .attr("width", chart.width())
     .attr("height", chart.height());
 
 d3.csv("viz.csv", function(error, csv) {
-	vis.datum(csv).call(chart);
+    vis.datum(csv).call(chart);
 });
-
-
-
-
-
-
